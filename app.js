ace1679c29b1e15534b3a5ae78c8af1be8ec7b05
Luiz Mendes <luiz.mendes@in8.com.br>
2021-09-09 13:31:50 -0300

    fix: change lib to get tun

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express')
+const ni = require('network-interfaces');
 var os = require( 'os' );
 const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  var networkInterfaces = os.networkInterfaces();
-  res.send(networkInterfaces)
+  const options = {
+    internal: false, // boolean: only acknowledge internal or external addresses (undefined: both)
+    ipVersion: 4     // integer (4 or 6): only acknowledge addresses of this IP address family (undefined: both)
+  };
+  var networkInterfaces = os.hostname();
+  const ip = ni.toIp('tuns1', options);
+  res.send(ip)
 })
 
 app.listen(port, () => {
