38dbdda294854f36ffed4d8aabbda050a0b31fc5
Luiz Mendes <luiz.mendes@in8.com.br>
2021-09-09 12:57:36 -0300

    chore: initial structure

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+var os = require( 'os' );
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  var networkInterfaces = os.networkInterfaces( );
+  res.send(networkInterfaces)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
