13c987836d95dc247a4435d80e5526c2e251aa0c
Luiz Mendes <luiz.mendes@in8.com.br>
2021-09-09 14:02:01 -0300

    teste

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 const express = require('express')
 const ni = require('network-interfaces');
 var os = require( 'os' );
+const si = require('systeminformation');
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
   const options = {
     internal: false, // boolean: only acknowledge internal or external addresses (undefined: both)
     ipVersion: 4     // integer (4 or 6): only acknowledge addresses of this IP address family (undefined: both)
   };
   var networkInterfaces = os.hostname();
   const ip = os.networkInterfaces({ all: true })
-  res.send(ip)
+
+  const aa = await si.networkInterfaces()
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
+
+  res.send(aa)
 })
 
 app.listen(port, () => {
