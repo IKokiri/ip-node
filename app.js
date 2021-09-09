1018bd6cfabaea60a1a2737ea669c35e7a49c487
Luiz Mendes <luiz.mendes@in8.com.br>
2021-09-09 13:04:01 -0300

    fix: add port to prodution

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 var os = require( 'os' );
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   var networkInterfaces = os.networkInterfaces( );
